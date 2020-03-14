4272a57966da3f61dd5c5f0f06af7bac053ed249
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-14 23:43:21 +0900

    Add DecimalToBinary

---
+++ src/java1/basic_algo/DecimalToBinary.java
@@ -0,0 +1,23 @@
+package java1.basic_algo;
+/**
+ * 10진수를 2진수로 변환하시오
+ */
+public class DecimalToBinary {
+    
+    public static void main(String[] args) {
+
+        int inputNum = 19;
+        int bin[] = new int[100];
+
+        /**
+         * 19/2 = 9 ... 1
+         * 9/2  = 4 ... 1
+         * 4/2  = 2 ... 0
+         * 2/2  = 1 ... 0
+         * 1/2  = 0 ... 1
+         * 
+         * 010011
+         */
+
+    }
+}
