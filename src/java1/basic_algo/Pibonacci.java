20685bcc08c2d85b98610e0f797c4e5f4aa9a86d
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-29 22:09:35 +0900

    Divede the solution method from main method

--- src/java1/basic_algo/Pibonacci.java
+++ src/java1/basic_algo/Pibonacci.java
@@ -5,11 +5,33 @@ package java1.basic_algo;
  * An = An-1 + An-2
  * 1 1 2 3 5 8 13 21 34 55
  */
-
 public class Pibonacci {
 
     public static void main(String[] args){
 
+        System.out.println(solution1(15));
+        System.out.println("================");
+        System.out.println(solution2(15));
+
+    }
+    
+    public static String solution1(int N){
+        
+        int ppNo = 1;
+        int pNo = 1;
+        String str=ppNo + " " + pNo;
+
+        for(int i=2; i<N; i++ ){
+            int no = ppNo+pNo;
+            str = str + " " + no;
+            ppNo=pNo;
+            pNo=no;
+        }
+        return str;
+    }
+
+    public static String solution2(int N){
+        String str="";
         int[] numsArr = new int[50];
         numsArr[0]=1;
         numsArr[1]=1;
@@ -19,22 +41,8 @@ public class Pibonacci {
         }
 
         for(int i=0; i<15; i++){
-            System.out.print(numsArr[i] + " ");
+            str = str + " " + numsArr[i];
         }
-
-        System.out.println("\n==========================");
-
-        int ppNo = 1;
-        int pNo = 1;
-        System.out.print(ppNo+" ");
-        System.out.print(pNo+" ");
-
-        for(int i=2; i<15; i++ ){
-            int no = ppNo+pNo;
-            System.out.print(no+" ");
-            ppNo = pNo;
-            pNo=no;
-        }
+        return str;
     }
-
 }
